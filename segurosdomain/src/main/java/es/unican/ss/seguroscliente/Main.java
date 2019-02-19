c08d88c2b07a1ea7eafe26034ae627e1f1a8db8b
Fernando Solar <fernando.solari@alumnos.unican.es>
2019-02-19 12:16:04 +0100

    Fusionadas clases principales

--- segurosdomain/src/main/java/es/unican/ss/seguroscliente/Main.java
+++ segurosdomain/src/main/java/es/unican/ss/seguroscliente/Main.java
@@ -1,6 +1,7 @@
 package es.unican.ss.seguroscliente;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,7 +10,13 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
+import org.xml.sax.SAXException;
+
+import es.unican.ss.segurosSAX.AseguradoraHandler;
 import es.unican.ss.segurosdomain.Aseguradora;
 import es.unican.ss.segurosdomain.Cliente;
 import es.unican.ss.segurosdomain.Seguro;
@@ -67,6 +74,26 @@ public class Main {
 			marshaller.marshal(aseguradora, new File("SegurosDomain_v2.xml"));
 
 		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			// Instanciar el parser
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+			
+			// Crear el manejador
+			AseguradoraHandler handler = new AseguradoraHandler();
+			
+			// Parsear el fichero
+			saxParser.parse("src/main/resources/SegurosDomain.xml", handler);
+			
+			System.out.println("Numero de clientes: " + handler.getNumClientes());
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
