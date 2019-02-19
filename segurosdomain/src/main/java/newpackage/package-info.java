6abed97b12aff736eba6233f3d33a3cbab5113d1
Fernando Solar <fernando.solari@alumnos.unican.es>
2019-02-19 12:35:42 +0100

    Eliminadas clases obsoletas

--- segurosdomain/src/main/java/newpackage/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2019.02.12 a las 12:50:50 PM CET 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.unican.es/ss/SegurosDomain", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package newpackage;
