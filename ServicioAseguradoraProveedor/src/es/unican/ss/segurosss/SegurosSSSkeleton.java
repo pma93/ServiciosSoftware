724e3401e74dd844e92bb4751f69c9f67165fb8a
Fernando Solar <fernando.solari@alumnos.unican.es>
2019-03-04 00:42:30 +0100

    Skeletons generados por wsdl2java

---
+++ ServicioAseguradoraProveedor/src/es/unican/ss/segurosss/SegurosSSSkeleton.java
@@ -0,0 +1,40 @@
+/**
+ * SegurosSSSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.9  Built on : Nov 16, 2018 (12:05:37 GMT)
+ */
+package es.unican.ss.segurosss;
+
+/**
+ *  SegurosSSSkeleton java skeleton for the axisService
+ */
+public class SegurosSSSkeleton {
+    /**
+     * Auto generated method signature
+     * Retorna el tipo, precio y matrícula del vehículo asociado a un ID de seguro
+     * @param retornarDatosSeguro
+     * @return retornarDatosSeguroResponse
+     * @throws DatosNoValidosException
+     */
+    public es.unican.ss.segurosss.types.InfoSeguro retornarDatosSeguro(
+        java.lang.String retornarDatosSeguro) throws DatosNoValidosException {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#retornarDatosSeguro");
+    }
+
+    /**
+     * Auto generated method signature
+     * Retorna la lista de seguros asociados al DNI de un cliente
+     * @param retornarListaSeguros
+     * @return retornarListaSegurosResponse
+     * @throws DatosNoValidosException
+     */
+    public es.unican.ss.segurosss.types.ListaSeguros retornarListaSeguros(
+        java.lang.String retornarListaSeguros) throws DatosNoValidosException {
+        //TODO : fill this with the necessary business logic
+        throw new java.lang.UnsupportedOperationException("Please implement " +
+            this.getClass().getName() + "#retornarListaSeguros");
+    }
+}
