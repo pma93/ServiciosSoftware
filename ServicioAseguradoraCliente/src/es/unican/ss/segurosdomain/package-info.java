a88f4984cbad64a4951aa04b8ddd676b6a017136
Fernando Solar <fernando.solari@alumnos.unican.es>
2019-03-05 13:01:17 +0100

    Generación automática del cliente

---
+++ ServicioAseguradoraCliente/src/es/unican/ss/segurosdomain/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.unican.es/ss/SegurosDomain", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package es.unican.ss.segurosdomain;
